429cfd8a1b0f66e3e02085875aec938b43f63dee
hayatosann <sasano.hayato@icloud.com>
2019-09-21 19:57:45 +0900

    AddingSmallPlanet&WriteDownMovingOfSmallPlanet

--- ShootingGame/GameScene.swift
+++ ShootingGame/GameScene.swift
@@ -12,8 +12,12 @@ import CoreMotion
 class GameScene: SKScene {
     let motionManager = CMMotionManager()
     var accelaration: CGFloat = 0.0
+    
+    var timer: Timer?
+    
     var earth: SKSpriteNode!
     var spaceship: SKSpriteNode!
+    
     override func didMove(to view: SKView) {
         self.earth = SKSpriteNode(imageNamed: "earth")
         self.earth.xScale = 1.5
@@ -31,22 +35,41 @@ class GameScene: SKScene {
             let a = data.acceleration
             self.accelaration = CGFloat(a.x) * 0.75 + self.accelaration * 0.25
         }
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { _ in
+            self.addAsteroid()
+        })
     }
+    
     override func didSimulatePhysics() {
         let nextPosition = self.spaceship.position.x + self.accelaration * 50
         if nextPosition > frame.width / 2 - 30 { return }
         if nextPosition < -frame.width / 2 + 30 { return }
         self.spaceship.position.x = nextPosition
     }
-    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         let missile = SKSpriteNode(imageNamed: "missile")
         missile.position = CGPoint(x: self.spaceship.position.x, y: self.spaceship.position.y + 50)
         addChild(missile)
-        
         let moveToTop = SKAction.moveTo(y: frame.height + 10, duration: 0.3)
         let remove = SKAction.removeFromParent()
         missile.run(SKAction.sequence([moveToTop, remove]))
+    }
+    
+    func addAsteroid() {
+        let names = ["asteroid1", "asteroid2", "asteroid3"]
+        let index = Int(arc4random_uniform(UInt32(names.count)))
+        let name = names[index]
+        let asteroid = SKSpriteNode(imageNamed: name)
+        let random = CGFloat(arc4random_uniform(UINT32_MAX)) / CGFloat(UINT32_MAX)
+        let positionX = frame.width * (random - 0.5)
+        asteroid.position = CGPoint(x: positionX, y: frame.height / 2 + asteroid.frame.height)
+        asteroid.scale(to: CGSize(width: 70, height: 70))
+        addChild(asteroid)
+        
+        let move = SKAction.moveTo(y: -frame.height / 2 - asteroid.frame.height, duration: 6.0)
+        let remove = SKAction.removeFromParent()
+        asteroid.run(SKAction.sequence([move, remove]))
     }
     
 }
