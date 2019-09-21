2c08efaba63aa1f7a289e935b6d94ef2393a3ef1
hayatosann <sasano.hayato@icloud.com>
2019-09-21 19:40:24 +0900

    WriteDownInOrderToDisplayStarShip

--- ShootingGame/GameScene.swift
+++ ShootingGame/GameScene.swift
@@ -8,9 +8,10 @@
 
 import SpriteKit
 import GameplayKit
-
 class GameScene: SKScene {
+    
     var earth: SKSpriteNode!
+    var spaceship: SKSpriteNode!
     
     override func didMove(to view: SKView) {
         self.earth = SKSpriteNode(imageNamed: "earth")
@@ -19,6 +20,11 @@ class GameScene: SKScene {
         self.earth.position = CGPoint(x: 0, y: -frame.height / 2)
         self.earth.zPosition = -1.0
         addChild(self.earth)
+        
+        self.spaceship = SKSpriteNode(imageNamed: "spaceship")
+        self.spaceship.scale(to: CGSize(width: frame.width / 5, height: frame.width / 5))
+        self.spaceship.position = CGPoint(x: 0, y: self.earth.frame.maxY + 50)
+        addChild(self.spaceship)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
